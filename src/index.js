52e507aca7125da78a11cff0dd585095b8d2d710
Rohmat MAC <rohmat661@gmail.com>
2023-01-18 17:57:34 +0700

    up

--- src/index.js
+++ src/index.js
@@ -3,17 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthContextProvider } from './context/authContext';
 import { DarkModeContextProvider } from './context/darkModeContext';
-import { HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <HashRouter>
-      <DarkModeContextProvider>
-        <AuthContextProvider>
-          <App />
-        </AuthContextProvider>
-      </DarkModeContextProvider>
-    </HashRouter>
+    <DarkModeContextProvider>
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
+    </DarkModeContextProvider>
   </React.StrictMode>
 );
