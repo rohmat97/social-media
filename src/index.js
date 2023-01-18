86a6e12c10c41fca42362749c6ccceba01275d7f
Rohmat MAC <rohmat661@gmail.com>
2023-01-18 12:07:40 +0700

    fix deploy

--- src/index.js
+++ src/index.js
@@ -3,14 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { AuthContextProvider } from './context/authContext';
 import { DarkModeContextProvider } from './context/darkModeContext';
+import { HashRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <DarkModeContextProvider>
-      <AuthContextProvider>
-        <App />
-      </AuthContextProvider>
-    </DarkModeContextProvider>
+    <HashRouter>
+      <DarkModeContextProvider>
+        <AuthContextProvider>
+          <App />
+        </AuthContextProvider>
+      </DarkModeContextProvider>
+    </HashRouter>
   </React.StrictMode>
 );
